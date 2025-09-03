3da8d9c823a307d9b498f814ac510a63563cee97
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-09-03 21:17:35 +0530

    correct urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -10,7 +10,7 @@ app_name = "polls"
 # ]
 
 
-urlPatters = [
+urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
